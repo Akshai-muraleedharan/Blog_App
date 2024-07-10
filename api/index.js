a5922951c74de3168a3aff37c5bf78899302191d
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-07-10 12:44:56 +0100

    db connection success

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 const dotenv =require('dotenv');
+const mongoose =require('mongoose')
 
 dotenv.config()
+ 
+
+
+app.get('/',(req,res)=>{
+    res.send("hello developer akshai")
+})
+
+
+    mongoose.connect(process.env.MONGODB_KEY)
+    .then(()=> console.log("DB connected successfully"))
+    .catch((err) => console.log(err))
 
 const port = process.env.PORT || 4001
 
 app.listen(port,()=> console.log(`server is connected on port: ${port}`))
-
+ 
 
 
 
